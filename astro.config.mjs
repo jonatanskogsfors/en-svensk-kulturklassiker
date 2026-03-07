771b41129c0726d129286364aab2a5ffa2b31088
Jonatan Skogsfors <jonatan@skogsfors.net>
2026-03-08 00:22:31 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-const isProd = process.env.GITHUB_ACTIONS === 'true';
-
 export default defineConfig({
   output: 'static',
   site: 'https://svensk-kulturklassiker.github.io',
-  base: isProd ? '/en-svensk-kulturklassiker/' : '/',
+  base: '/',
 });
